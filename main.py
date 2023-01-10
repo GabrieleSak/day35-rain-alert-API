6384225f644c1eafa7b9a1a93c6867d6648b4f1c
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-01-10 14:50:47 +0200

    Print "Bring Umbrella" if any of 5 forecasts with data every 3 hours has weather condition code less than 700

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from api_key import api_key
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
-
 parameters = {
     "lat": 54.687157,
     "lon": 25.279652,
     "appid": api_key,
     "units": "metric",
-    "cnt": 17
+    "cnt": 5
 }
 response = requests.get(OWM_Endpoint, params=parameters)
 response.raise_for_status()
-print(response.status_code)
-data = response.json()
-print(data)
+weather_data = response.json()
+weather_conditions = []
+
+for weather_hourly in weather_data["list"]:
+    for item in weather_hourly["weather"]:
+        weather_conditions.append(item["id"])
+
+for item in weather_conditions:
+    if item < 700:
+        print("Bring an Umbrella")
+        break
